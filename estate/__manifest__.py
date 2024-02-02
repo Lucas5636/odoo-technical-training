442d7e0d45ba610c7ec8818903c48c614e339265
Lucas5636 <etu42975@henallux.be>
2024-02-02 11:28:09 +0100

    module minimum

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,9 +1,13 @@
 {
     "name": "Estate",  # The name that will appear in the App list
     "version": "16.0.0",  # Version
+    "author": "Langue Lucas",
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
+
+    ],
+    'demo':[
 
     ],
     "installable": True,
