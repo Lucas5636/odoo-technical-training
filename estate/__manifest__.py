183f5897eb44c43262004c15ad98a0d79b060905
Lucas5636 <etu42975@henallux.be>
2024-02-07 15:47:37 +0100

    search, filter, groupBy et début estate_property_type (uniquement le fichier py)

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,8 +7,7 @@
     "data": [
         "security/ir.model.access.csv",
         "views/estate_property_views.xml",
-	    "views/estate_menus.xml",
-        "views/estate_property_tree_views.xml",
+	"views/estate_menus.xml",
     ],
     'demo':[
 
---
+++ estate/models/estate_property_type.py
@@ -0,0 +1,9 @@
+#---External---
+from dateutil.relativedelta import relativedelta
+#---Odoo---
+from odoo import models, fields
+
+class EstatePropertyType(models.Model):
+#---Methods---
+
+#---Data---
