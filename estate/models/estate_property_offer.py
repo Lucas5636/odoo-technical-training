6ca7829d6ec69e8c8357a1a7e176ff2d7b7257c8
Lucas5636 <etu42975@henallux.be>
2024-02-09 10:30:22 +0100

    Fin du chapitre 8 Odoo, prochaine étape ajouter des offres dans bd

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -8,4 +8,14 @@ class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = "Offre de propriété de l'immobilier"
     #---Data---
-    name = fields.Char("Nom", required=True)
+    price = fields.Float("Prix")
+    status = fields.Selection(
+            selection=[
+                ("accepted", "Acceptée"),
+                ("refused", "Refusée"),
+            ],
+        string="Status",
+        copy=False,
+    )
+    partner_id = fields.Many2one("res.partner", string="Partenaire", required=True)
+    property_id = fields.Many2one("estate.property", string="Propriété", required=True)
--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -3,3 +3,4 @@ from odoo import models, fields
 class Users(models.Model):
     _inherit = 'res.users'
 
+    #--- Voir si déjà nécessaire car res.partner fonctionne sans fichier ---
