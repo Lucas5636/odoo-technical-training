a675e27098fb7d5790b6f0266194d787e6e9a578
Lucas5636 <etu42975@henallux.be>
2024-02-12 15:42:18 +0100

    Ajout de séquence pour organiser manuellement les types de propriétées

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -6,7 +6,7 @@ class EstatePropertyType(models.Model):
     #---Private ---
     _name = "estate.property.type"
     _description = "Type de propriété de l'immobilier"
-    _order = "name"
+    _order = "sequence, name"
     # ---SQL Constraints---
     _sql_constraints = [
         ("check_name", "UNIQUE(name)",
@@ -15,4 +15,6 @@ class EstatePropertyType(models.Model):
     # ---Methods---
     #---Data---
     name = fields.Char("Nom", required=True)
+    sequence = fields.Integer("Séquence")
+    #---Relations---
     property_ids = fields.One2many("estate.property", "property_type_id", string="Propriétés")
