f82befc4ed4b41c5ee8327a75af707fc92408c3b
Lucas5636 <etu42975@henallux.be>
2024-02-19 15:21:02 +0100

    res_user fonctionnel pas encore dans une vue

--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -1,7 +1,7 @@
 from odoo import models, fields
 
-class Users(models.Model):
-    _inherit = 'res.users'
+class ResUsers(models.Model):
+    _inherit = "res.users"
 
     property_ids = fields.One2many("estate.property", "user_id", string="Propirétés", index=True,
-                                   domain="[('state', '=', ('new', 'offer_received')]")
+                                   domain=[("state", "=", ["new", "offer_received"])])
