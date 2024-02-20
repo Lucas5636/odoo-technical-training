cf4c67512f5f192e1f0a4f3bf910979f8a949dc4
Lucas5636 <etu42975@henallux.be>
2024-02-20 15:53:52 +0100

    Ajout du nom, de la qtté et du prix unitaire + ajout journal_id

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -2,17 +2,26 @@
 import logging
 _logger = logging.getLogger(__name__)
 #---Odoo---
-from odoo import models
+from odoo import models, Command
 class EstateProperty(models.Model):
     # ---Private ---
     _inherit = "estate.property"
 
     def action_sold(self):
         _logger.info("Hello from inherited action sold")
+        journal = self.env["account.journal"].search([("type", "=", "sale")], limit=1)
         for property in self:
             self.env["account.move"].create({
                 "partner_id": property.buyer_id.id,
                 "move_type": "out_invoice",
-                }
-            ),
+                "journal_id": journal.id,
+                "invoice_line_ids": [
+                    (0, 0, {
+                            "name": property.name,
+                            "quantity": 1.0,
+                            "price_unit": property.selling_price * 0.6,
+                        }
+                    )
+                ],
+            })
         return super().action_sold()
