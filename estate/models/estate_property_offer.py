1e0c2f45090748b2a4b189481df503d48edee45b
Lucas5636 <etu42975@henallux.be>
2024-02-21 09:27:04 +0100

    Correction estate_property_offer.py

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,21 +1,21 @@
-#---External---
+# ---External---
 from dateutil.relativedelta import relativedelta
-#---Odoo---
-from odoo import models, fields, api
+# ---Odoo---
+from odoo import api, models, fields
 from odoo.exceptions import UserError
 
 class EstatePropertyOffer(models.Model):
-    #---Private attributes---
+    # ---Private attributes---
     _name = "estate.property.offer"
-    _description = "Offre de propriété de l'immobilier"
+    _description = "Offre de propriétés immobilières"
     _order = "price desc"
-    #---SQL Constraints---
+    # ---SQL Constraints---
     _sql_constraints = [
         ("check_price", "CHECK(price > 0)",
          "Le prix de l'offre devrait être supérieur à 0"),
     ]
     # ---Methods---
-    #---Data---
+    # ---Data---
     price = fields.Float("Prix")
     status = fields.Selection(
             selection=[
@@ -26,13 +26,14 @@ class EstatePropertyOffer(models.Model):
         copy=False,
         readonly=True,
     )
-    validity = fields.Integer("Validité (jour(s))", default=7)
+    validity = fields.Integer("Validitée (jour(s))", default=7)
     date_deadline = fields.Date(string="Date limite", compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+    # ---Relations---
     partner_id = fields.Many2one('res.partner', string="Partenaire", required=True)
     property_id = fields.Many2one("estate.property", string="Propriété", required=True)
     property_type_id = fields.Many2one('estate.property.type', string='Property Type',
                                        related='property_id.property_type_id', store=True)
-
+    # ---Compute and Search---
     @api.depends("create_date", "validity")
     def _compute_date_deadline(self):
         for dates in self:
@@ -42,7 +43,7 @@ class EstatePropertyOffer(models.Model):
         for dates in self:
             date = dates.create_date.date() if dates.create_date else fields.Date.today()
             dates.validity = (dates.date_deadline - date).days
-    #---Action methods---
+    # ---Action methods---
     def action_accept(self):
         if "accepted" in self.mapped("property_id.offer_ids.status"):
             raise UserError("Une offre a déjà été acceptée")
