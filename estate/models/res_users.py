d83dfb7568b4584cf49665eab0d5043830c12937
Lucas5636 <etu42975@henallux.be>
2024-02-21 10:01:13 +0100

    Correction res_users.py

--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -1,7 +1,8 @@
+# ---Odoo---
 from odoo import models, fields
-
 class ResUsers(models.Model):
+    # ---Private attributes---
     _inherit = "res.users"
-
+    # ---Relations---
     property_ids = fields.One2many("estate.property", "user_id", string="Propirétés", index=True,
                                    domain=[("state", "in", ["new", "offer_received"])])
